7660409a76d02d45a106879dfa353eba0bb48e72
CDE-LAB3-PC28 <CDE-LAB3-PC28@FERNANDOUNINORT>
2022-08-10 21:25:19 -0400

    Inicio de Aplicacion

---
+++ src/main/java/com/villaverde/ejerciciouno/EjercicioUno.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.villaverde.ejerciciouno;
+
+/**
+ *
+ * @author CDE-LAB3-PC28
+ */
+public class EjercicioUno {
+
+    public static void main(String[] args) {
+        System.out.println("Elian villaverde!");
+    }
+}
---
+++ src/main/java/com/villaverde/ejerciciouno/HorarioDeClases.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.villaverde.ejerciciouno;
+
+/**
+ *
+ * @author CDE-LAB3-PC28
+ */
+public class HorarioDeClases {
+
+    public static void main(String[] args) {
+        System.out.println("Lunes\tMartes\tMierc.\tJueves\tViernes!");
+          System.out.println("=====\t=====\t=====\t=====\t=====!");
+           System.out.println("Pro.In\tArq\tPro.W\tAud.Sis\tIng.So!");
+    }
+}
+    
